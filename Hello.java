b0d9ab5ee5b84a8ca5a2ce4119e925e54c375caf
Fraser <fraselil27@7hills.org>
2026-09-22 10:30:10 -0400

    did an int and print statement

--- Hello.java
+++ Hello.java
@@ -2,6 +2,9 @@ public class Hello
 {
 	public static void main(String[] args)
 	{
-		System.out.print("Hello, world!");
+		System.out.println("Hello, world!");
+		System.out.println("It's a wonderful day in the neighborhood!");
+		int pizza = 8;
+		System.out.print(""+pizza+" pieces of pizza!");
 	}
 }
